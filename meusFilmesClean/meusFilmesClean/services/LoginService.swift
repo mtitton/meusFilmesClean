e0674fc8c017b4b78c6b0845b29f11bb0cf5ea6a
Marcus Titton <mtitton@MacBook-Pro-de-Marcus.local>
2019-10-01 14:27:32 -0300

    Inicio de projeto com o CLEAN

---
+++ meusFilmesClean/meusFilmesClean/services/LoginService.swift
@@ -0,0 +1,13 @@
+//
+//  LoginService.swift
+//  meusFilmesClean
+//
+//  Created by Marcus Titton on 01/10/19.
+//  Copyright © 2019 Marcus Titton. All rights reserved.
+//
+
+import Foundation
+
+class LoginService {
+    
+}
